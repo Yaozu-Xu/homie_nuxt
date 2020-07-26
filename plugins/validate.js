b4d8bd1371aca428a8caf5efe8ceb1853d9c7dff
xuyaozu <2589231996@qq.com>
2020-07-26 13:44:56 +0800

    update validate message

--- plugins/validate.js
+++ plugins/validate.js
@@ -4,7 +4,7 @@ import { required, email } from 'vee-validate/dist/rules.umd'
 
 extend('email', {
   ...email,
-  message: '邮箱不合法'
+  message: 'The email is illeagl'
 })
 
 extend('required', {
@@ -16,14 +16,14 @@ extend('articleName', (value) => {
   if(value.length >= 1 && value.length <= 20) {
     return true
   }
-  return '文章名长度不得>20'
+  return 'length should less than 20'
 })
 
 extend('user', (value) => {
   if(value.length >= 3 && value.length <= 10) {
     return true
   }
-  return '用户名长度必须在3-10之间'
+  return 'length should between 3 and 10'
 })
 
 Vue.component('ValidationProvider', ValidationProvider)

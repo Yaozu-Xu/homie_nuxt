3d680ac099b9a7ede27803f52dc2e647d07a0640
xuyaozu <2589231996@qq.com>
2020-08-16 19:21:39 +0800

    feat: add interceptor

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,15 @@
+import { getStore } from '~/utils'
+
 export default function({ $axios, redirect }) {
   $axios.setBaseURL('http://127.0.0.1:3001')
-  // $axios.onRequest((config) => {
-  //   console.log('Making request to ' + config.url)
-  // })
+  $axios.onRequest(config => {
+    const accessToken = getStore('access_token')
+    if (accessToken) {
+      config.header.access_token = accessToken
+    }
+    return config
+  })
+
   $axios.onError(error => {
     const code = parseInt(error.response && error.response.status)
     if (code === 400) {
